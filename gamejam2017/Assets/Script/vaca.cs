c6d712e260d8f3cf221c30850646f6d621716662
Carlos Sanches <carlos-caps@hotmail.com>
2017-09-17 12:14:48 -0300

    Add files via upload

---
+++ gamejam2017/Assets/Script/vaca.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class vaca : MonoBehaviour {
+    public Text txtLevel;
+    public Text txtMoedas;
+    public Text txtHumor;
+
+
+
+    // Use this for initialization
+    void Start () {
+        txtLevel.text = "Level" + GameManager.instance.level + " concluido";
+        txtMoedas.text = "Moedas: " + GameManager.instance.money;
+        txtHumor.text = "Humor: " + GameManager.instance.happy;
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+
+    public void Cena()
+    {
+
+        SceneManager.LoadScene("Escolha");
+    }
+}
